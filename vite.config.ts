4a65aa3c8af20cd19ccd574bc2db3d3468fc0f17
megapihar6 <karvasarniy.sa@intechs.by>
2025-10-13 23:54:46 +0200

    start

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: './',
+  base: '/deffina/',
   plugins: [react(), Pages(), tailwindcss(), svgr()],
 });
