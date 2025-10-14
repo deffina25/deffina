7d2917cc477441ca6a1633051bda91ec4ccdb2cc
megapihar6 <karvasarniy.sa@intechs.by>
2025-10-14 23:39:30 +0200

    Updates

---
+++ assets/formatDate-CqFHi3ty.js
@@ -0,0 +1,1 @@
+const t=e=>new Date(e).toLocaleDateString("en-US",{year:"numeric",month:"long",day:"numeric"});export{t as f};
---
+++ assets/team1-DCKPdNob.js
@@ -0,0 +1,1 @@
+const a="/deffina/assets/team1-DLSU4Swa.png";export{a as T};
